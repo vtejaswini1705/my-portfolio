81cdc2d00ee01004abc69fd05860f381553b56d6
vtejaswini1705 <vtejaswini1705@gmail.com>
2025-09-29 19:48:33 -0400

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -15,7 +15,7 @@ function Main() {
         <div className="content">
           <div className="social_icons">
             <a href="https://github.com/vtejaswini1705" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-            <a href="https://www.linkedin.com/in/vtejaswini1705/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+            <a href="www.linkedin.com/in/tejaswini-v-15b851387" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
           <h1>Tejaswini V</h1>
           <p>Data Engineer & Analyst | 3+ Years Experience</p>
