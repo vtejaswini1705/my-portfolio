c0488ca33544bfca4c8fdf90a9d655cd55e82e02
vtejaswini1705 <vtejaswini1705@gmail.com>
2025-09-29 19:56:43 -0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ function Footer() {
     <footer>
       <div>
         <a href="https://github.com/vtejaswini1705" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-        <a href="www.linkedin.com/in/tejaswini-v-15b851387" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+        <a href="https://www.linkedin.com/in/tejaswini-v-15b851387/" rel="noreferrer"><LinkedInIcon/></a>
       </div>
       <p>A portfolio designed & built by <a href="https://github.com/vtejaswini1705" target="_blank" rel="noreferrer">Tejaswini V</a> with 💜</p>
     </footer>
